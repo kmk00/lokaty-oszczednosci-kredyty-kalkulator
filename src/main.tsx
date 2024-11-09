9db342ddd329593ad30b40cf729acb11cd8a3fb0
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-11-09 21:55:25 +0100

    Configure vitest, add first basic test for the App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import LokatyContent from "./components/LokatyContent.tsx";
 import OszczednosciContent from "./components/OszczednosciContent.tsx";
 import KredytyContent from "./components/KredytyContent.tsx";
@@ -40,9 +44,11 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider
-      future={{ v7_startTransition: true }}
-      router={router}
-    ></RouterProvider>
+    <BrowserRouter>
+      <RouterProvider
+        future={{ v7_startTransition: true }}
+        router={router}
+      ></RouterProvider>
+    </BrowserRouter>
   </StrictMode>
 );
