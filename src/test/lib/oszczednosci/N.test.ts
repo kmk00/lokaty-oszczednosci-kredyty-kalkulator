7985dca59c95ec1de1e2301f080c30327cb7134d
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-11-23 17:15:15 +0100

    Add tests for N calculations

--- src/test/lib/oszczednosci/N.test.ts
+++ src/test/lib/oszczednosci/N.test.ts
@@ -10,21 +10,115 @@ import {
 } from "../../../lib/oszczednosci";
 
 describe("Test model #1", () => {
-  it.todo("Should calculate the correct N value from up");
-  it.todo("Should calculate the correct N value from down");
+  it("Should calculate the correct N value from up", () => {
+    const data = {
+      E: 1,
+      r: 0.1,
+      Kn: 5.11,
+    };
+    const model = new NModel1Up();
+
+    const result = model.calculate(data);
+
+    expect(result).toBe(4);
+  });
+  it("Should calculate the correct N value from down", () => {
+    const data = {
+      E: 1,
+      r: 0.1,
+      Kn: 4.64,
+    };
+    const model = new NModel1Down();
+
+    const result = model.calculate(data);
+
+    expect(result).toBe(4);
+  });
 });
 
 describe("Test model #2", () => {
-  it.todo("Should calculate the correct N value from up");
-  it.todo("Should calculate the correct N value from down");
+  it("Should calculate the correct N value from up", () => {
+    const data = {
+      E: 1,
+      r: 0.1,
+      m: 3,
+      Kn: 2.32,
+    };
+    const model = new NModel2Up();
+
+    const result = model.calculate(data);
+
+    expect(result).toBe(2);
+  });
+  it("Should calculate the correct N value from down", () => {
+    const data = {
+      E: 1,
+      r: 0.1,
+      m: 3,
+      Kn: 2.1,
+    };
+    const model = new NModel2Down();
+
+    const result = model.calculate(data);
+
+    expect(result).toBe(2);
+  });
 });
 
 describe("Test model #3", () => {
-  it.todo("Should calculate the correct N value from up");
-  it.todo("Should calculate the correct N value from down");
+  it("Should calculate the correct N value from up", () => {
+    const data = {
+      E: 1,
+      r: 0.1,
+      m: 3,
+      Kn: 6.74,
+    };
+    const model = new NModel3Up();
+
+    const result = model.calculate(data);
+
+    expect(result).toBe(2);
+  });
+  it("Should calculate the correct N value from down", () => {
+    const data = {
+      E: 1,
+      r: 0.1,
+      m: 3,
+      Kn: 6.52,
+    };
+    const model = new NModel3Down();
+
+    const result = model.calculate(data);
+
+    expect(result).toBe(2);
+  });
 });
 
 describe("Test model #4", () => {
-  it.todo("Should calculate the correct N value from up");
-  it.todo("Should calculate the correct N value from down");
+  it("Should calculate the correct N value from up", () => {
+    const data = {
+      E: 1,
+      r: 0.1,
+      m: 3,
+      Kn: 6.72,
+    };
+    const model = new NModel4Up();
+
+    const result = model.calculate(data);
+
+    expect(result).toBe(2);
+  });
+  it("Should calculate the correct N value from down", () => {
+    const data = {
+      E: 1,
+      r: 0.1,
+      m: 3,
+      Kn: 6.51,
+    };
+    const model = new NModel4Down();
+
+    const result = model.calculate(data);
+
+    expect(result).toBe(2);
+  });
 });
